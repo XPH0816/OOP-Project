cbad02d1479f09392b8dc0986787406380ac6aeb
Lim Shi Song <limshisong123@gmail.com>
2023-12-08 20:51:01 +0800

    Update run.bat to include compilation and copying of source files

--- compile.bat
+++ compile.bat
@@ -2,4 +2,3 @@
 if not exist bin mkdir bin else rd /S /Q bin
 javac -sourcepath src -d bin -cp "lib/*" -encoding UTF-8 src/App.java
 copy src\.e* bin
-java -XX:+ShowCodeDetailsInExceptionMessages -cp "bin;lib/*" App
